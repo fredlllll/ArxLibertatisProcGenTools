59163e4bbb023cc6bd48937a4f2e5949c50226c7
Frederik Gelder <frederik.gelder@freenet.de>
2021-10-16 21:55:10 +0200

    back to step 1

---
+++ ArxLibertatisProcGenTools/Program.cs
@@ -0,0 +1,18 @@
+﻿using ArxLibertatisEditorIO;
+using ArxLibertatisEditorIO.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArxLibertatisProcGenTools
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArxPaths.DataDir = @"F:\Program Files\Arx Libertatis";
+
+
+        }
+    }
+}
--- ArxLibertatisProcGenTools/Util.cs
+++ ArxLibertatisProcGenTools/Util.cs
@@ -6,14 +6,6 @@ namespace ArxLibertatisProcGenTools
 {
     public static class Util
     {
-        public static char[] StringToChars(string str, int arrayLength)
-        {
-            char[] retval = new char[arrayLength];
-            for(int i =0; i< Math.Min(str.Length, arrayLength); i++)
-            {
-                retval[i] = str[i];
-            }
-            return retval;
-        }
+        
     }
 }
